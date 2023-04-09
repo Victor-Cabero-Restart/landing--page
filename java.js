0f2f20a7ca985729de53b2d59fde9d8664f34597
Victor-Cabero-Restart <127516373+Victor-Cabero-Restart@users.noreply.github.com>
2023-04-09 23:13:34 +0200

    Add files via upload

---
+++ java.js
@@ -0,0 +1,29 @@
+// let sky="clémentiño el medecin ";
+// let nom="dias Costa";
+// const salut= `hola ${sky}, tu t'appelles ${nom}`; 
+
+// const capitalize= (mot) => {
+//     return mot [0].toUpperCase( ) + mot.substring(1)
+
+// }
+
+// console.log(capitalize(sky), capitalize(nom));
+
+// function evenOrOdd (number) { 
+//     if (number%2==0){
+//         return "even"
+//     }
+//     else {
+//         return "odd"
+//     }
+// }
+
+// function sumOfNegative(number) {
+//     result=0
+//     number.forEach(element => {
+//         if (element<=0) {
+//             result+=element
+//         }
+//      });
+//     return result
+// }
